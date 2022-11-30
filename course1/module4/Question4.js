2f87452faa8b2d58312b6067252705c538980ab5
josh <you@example.com>
2022-11-30 17:06:38 +0530

    changes done

--- course1/module4/Question4.js
+++ course1/module4/Question4.js
@@ -7,6 +7,13 @@ console.log(typeof JSON.parse('{"Harshal" : "sonawane"}'))
 
 //for invalid
 //console.log(typeof JSON.parse('{"Harshal" = "sonawane"}'))
+console.log( JSON.parse('{"Harshal" : "sonawane"}')) //valid
+console.log(JSON.parse(null))  //valid for null ...and typeof null is also object.
+console.log( JSON.parse(3))   //valid for number
+console.log( JSON.parse(undefined))  //invalid
+console.log( JSON.parse('{"abc":""}'))  //valid for empty string also.
+console.log(JSON.parse(function (a){}))  //invalid
+console.log(typeof JSON.parse(Date));  //invalid coz date also fucntion.
 
 console.log( JSON.parse('{"Harshal" : "sonawane"}')) //valid
 console.log(JSON.parse(null))  //valid for null ...and typeof null is also object.
