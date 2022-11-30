dff1ee722a3a03d90d2a8b549b6dd87b8cd8f216
josh <you@example.com>
2022-11-30 17:42:39 +0530

    resolved conflicts

--- course1/module4/Question2.js
+++
@@ -1,9 +0,0 @@
-//2.What do you think would happen if you pass an index beyond the range of the string? Or if you pass a negative index? Try it out.
-
-
-let str1 = "Harshal";
-console.log(str1.charAt(7)); // If we pass index out of range then  it eill returns empty string.
-
-
-let str2 = "Harshal";
-console.log(str2.charAt(-1)); // If we pass negative index it eill returns also empty string.
