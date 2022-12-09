16535ebf6152d4165245155e69b5ac4aff71b444
josh <you@example.com>
2022-12-09 12:39:39 +0530

    changes is done Q1 and send number of milliseconds as an argument

--- course1/module8/Question1.js
+++ course1/module8/Question1.js
@@ -1,11 +1,11 @@
 //1. Write a function that can stop execution of a function for the number of milliseconds sent as an argument
 
-const func = async () => {
+const func = async (milliseconds) => {
   console.log("Printing before");
   //Call your function here eg. sleep(3000)
   setTimeout(function () {
     console.log("Printing after");
-  }, 3000);
+  }, milliseconds);
 };
 
-func();
+func(3000);
