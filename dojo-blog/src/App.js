39218d68b688bd67f8fa4cef9ae9cc5658dac3fc
josh <you@example.com>
2022-12-28 12:59:40 +0530

    added CRUD operations and loader in assignment 2

---
+++ dojo-blog/src/App.js
@@ -0,0 +1,37 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Create from "./Create";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./notFound";
+import Edit from "./Edit";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="/edit/:id">
+              <Edit />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
